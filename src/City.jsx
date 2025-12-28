b28cf5193769f55cf5d3464699449276ca24a7e1
yattaah <yattaah@gmail.com>
2025-12-28 11:58:37 +0100

    changed camera transition, cleanup

--- src/City.jsx
+++ src/City.jsx
@@ -1,6 +1,6 @@
-import { useGLTF, OrbitControls, Environment, meshBounds, CameraControls } from "@react-three/drei";
+import { useGLTF, OrbitControls, Environment, meshBounds, CameraControls, Billboard, Text, Html } from "@react-three/drei";
 import { useLoader, useFrame, useThree } from '@react-three/fiber'
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
 import {N8AOPass} from "n8ao"
 import { EffectComposer } from "@react-three/postprocessing"
@@ -8,7 +8,7 @@ import * as THREE from "three"
 
 
 
-
+///////////////// MAIN CITY COMPONENT /////////////////
 function City() {
 
   const cameraControls = useRef()
@@ -18,73 +18,54 @@ function City() {
   const isAnimating = useRef(false);
 
 
+  /////// USE FRAME///////
   useFrame(() => {
 
-    /////// CAMERA TRANSITION
-    if (!cameraTarget.current) return
+    /////// CAMERA TRANSITION ///////
+    if (isAnimating.current == true) {
 
     camera.position.lerp(cameraTarget.current.position, 0.05)
     cameraControls.current.target.lerp(cameraTarget.current.lookAt, 0.05)
     cameraControls.current.update()
 
-    // STOP CONDITION
-    if (
-      camera.position.distanceTo(cameraTarget.current.position) < 0.01
-    ) {
+    if (camera.position.distanceTo(cameraTarget.current.position) < 0.2) {
       cameraTarget.current = null
-      isAnimating.current = false
       cameraControls.current.enabled = true
+      isAnimating.current = false
     }
 
-
+    }
   })
 
-    // const model = useLoader(GLTFLoader, "./models/city.glb")
+    /////// MODEL LOADING ///////
     const model = useGLTF("./models/city.glb")
-
-    const cube = useRef();
 
     const eventHandlerCube = (event) => {
       event.stopPropagation();
       cube.current.material.color.set(`hsl(${Math.random() * 360}, 100%, 50%)`);
-      // console.log(" ---- POSITION")
-      // console.log(event.position);
-      // console.log(cube.current.material.color)
-
       console.log(event);
       console.log("X: " + event.x)
     }
 
+
+    /////// EVENT HANDLER ///////
     const buildingDoubleClickHandler = (event) => {
-          // console.log(event.object)
           event.stopPropagation();
           event.object.material.color.set(`hsl(${Math.random() * 360}, 100%, 50%)`)
-          console.log("----- POSITION");
+          console.log(event.object.name);
           console.log(event.object.position);
 
           const objectPosition = event.object.position.clone()
 
           cameraTarget.current = {
-          position: objectPosition.clone().add(new THREE.Vector3(2, 2, 2)),
+          position: objectPosition.clone().add(new THREE.Vector3(1.5, 1.8, 2)),
           lookAt: objectPosition.clone()
   }
 
-  isAnimating.current = true
-  cameraControls.current.enabled = false
+          // set states to disable camera controls
+          isAnimating.current = true
+          cameraControls.current.enabled = false
 
-  // // Move camera slightly away from the object (important!)
-  // camera.position.set(
-  //   objectPosition.x + 2,
-  //   objectPosition.y + 2,
-  //   objectPosition.z + 2
-  // )
-
-  // // Look at the object
-  // camera.lookAt(objectPosition)
-
-  // Update OrbitControls target
-  // cameraControls.current.target.copy(objectPosition)
-  // cameraControls.current.update()
 
     }
 
@@ -92,46 +73,27 @@ function City() {
 
   return (
     <>
-    <EffectComposer>
-{/* const composer = new EffectComposer(renderer);
-    // N8AOPass replaces RenderPass
-    const n8aopass = new N8AOPass(
-            scene,
-            camera,
-            width,
-            height
-        ); */}
-    composer.addPass(n8aopass);
-      </EffectComposer>
-
-
-
 
       <OrbitControls ref={cameraControls}/>
-      {/* <CameraControls /> */}
       <Environment background preset="city" />
-      <mesh>
-        {/* <boxGeometry args={[2, 1]} /> */}
-      </mesh>
-
-      <mesh 
-        ref = {cube} 
-        position-y = {1} 
-        onDoubleClick={eventHandlerCube} 
-        raycast={meshBounds}
-        onPointerEnter = {() => {document.body.style.cursor = "pointer"}}
-        onPointerLeave = {() => {document.body.style.cursor = "default"}}>
-        <boxGeometry />
-        <meshStandardMaterial roughness={0.15} color="red"/>
-      </mesh>
-
 
       <primitive 
         object={model.scene} 
         onDoubleClick = {buildingDoubleClickHandler}
+        // onPointerEnter = {() => {document.body.style.cursor = "pointer"}}
+        // onPointerLeave = {() => {document.body.style.cursor = "default"}}
       />
 
-      
+      {/* <Billboard
+        follow={true}
+        lockX={false}
+        lockY={false}
+        lockZ={false} // Lock the rotation on the z axis (default=false)
+        position={[0, 2, 0]}
+      >
+        <Text fontSize={1}>I'm a billboard</Text>
+      </Billboard> */}
+
     </>
   );
 }
